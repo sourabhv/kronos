f72d2777f25e92d5f6080e8c807bc94c08a902f7
Sourabh Verma <sourabh.coder@gmail.com>
2014-04-03 19:58:40 +0530

    Fix settings/base.py

--- pygameWorkshopRegistration/settings/base.py
+++ pygameWorkshopRegistration/settings/base.py
@@ -121,16 +121,7 @@ LOCAL_APPS = (
     'registrar'
 )
 
-
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.admin',
-)
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to

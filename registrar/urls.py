05de67c4e950cfd113d1051e945460b2a2c456e6
Sourabh Verma <sourabh.coder@gmail.com>
2014-04-07 20:33:36 +0530

    Add static url

--- registrar/urls.py
+++ registrar/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import patterns, url
+from django.conf import settings
 
 from .views import index
 
 urlpatterns = patterns('',
     url(r'^$', index, name='index'),
 )
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.contrib.staticfiles.views', {'document_root': settings.STATIC_ROOT}),
+    )

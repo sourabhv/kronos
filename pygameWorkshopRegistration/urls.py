6780886b271c48f3dc49d49b60bd4a00d870fdae
Sourabh Verma <sourabh.coder@gmail.com>
2014-04-12 15:07:18 +0530

    Add /static url in main URLCONF

--- pygameWorkshopRegistration/urls.py
+++ pygameWorkshopRegistration/urls.py
@@ -1,9 +1,16 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('registrar.urls', namespace='registrar')),
+)
+
+urlpatterns += patterns('',
+    (r'^static/(.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT
+    }),
 )

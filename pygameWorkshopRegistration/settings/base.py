5b8525a08de83e73916a98780abc481fe0147d04
Sourabh Verma <sourabh.coder@gmail.com>
2014-04-13 19:00:02 +0530

    Fix herokuapp /static 500 error problem - Use dj-static, static to host static files - use gunicorn to run django

--- pygameWorkshopRegistration/settings/base.py
+++ pygameWorkshopRegistration/settings/base.py
@@ -60,7 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = root('..', 'static')
+STATIC_ROOT = 'staticfiles'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -68,7 +68,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    root('static'),
+    # root('static'),
 )
 
 # List of finder classes that know how to find static files in
--- registrar/urls.py
+++ registrar/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, url
-from django.conf import settings
 
 from .views import index, register
 
@@ -7,8 +6,3 @@ urlpatterns = patterns('',
     url(r'^$', index, name='index'),
     url(r'^pyworkshop/$', register, name='register'),
 )
-
-if not settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.contrib.staticfiles.views', {'document_root': settings.STATIC_ROOT}),
-    )

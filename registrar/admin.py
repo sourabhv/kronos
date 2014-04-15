3c3e6af02e44a79c467682e671df360e33e2b4e9
Sourabh Verma <sourabh.coder@gmail.com>
2014-04-15 20:53:19 +0530

    Style form and change experience_in_python to options menu

--- registrar/admin.py
+++ registrar/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from .models import Applicant
 
 class ApplicantAdmin(admin.ModelAdmin):
-    fields = ('registered_at', 'name', 'semester', 'exprienced_in_python',
+    fields = ('registered_at', 'name', 'semester', 'python_exprience',
               'phone_number', 'email', 'solved_puzzle',)
     readonly_fields = ('registered_at',)
-    list_display = ('name', 'semester', 'exprienced_in_python', 'solved_puzzle',)
+    list_display = ('name', 'semester', 'python_exprience', 'solved_puzzle',)
     list_filter = ('registered_at', 'semester', 'solved_puzzle',)
 
 admin.site.register(Applicant, ApplicantAdmin)

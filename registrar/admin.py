539c20eef547a3f78082eee12ed3e1dc9a563ef3
Sourabh Verma <sourabh.coder@gmail.com>
2014-10-30 23:18:59 +0530

    Update Applicant form

--- registrar/admin.py
+++ registrar/admin.py
@@ -3,9 +3,9 @@ from .models import Applicant
 
 class ApplicantAdmin(admin.ModelAdmin):
     fields = ('workshop', 'registered_at', 'name', 'year',
-              'python_experience', 'phone_number', 'email', 'solved_puzzle',)
+              'phone_number', 'email', 'solved_puzzle',)
     readonly_fields = ('registered_at',)
-    list_display = ('workshop', 'name', 'year', 'python_experience', 'solved_puzzle',)
+    list_display = ('workshop', 'name', 'year', 'solved_puzzle',)
     list_filter = ('workshop', 'registered_at', 'year', 'solved_puzzle',)
 
 admin.site.register(Applicant, ApplicantAdmin)
--- registrar/models.py
+++ registrar/models.py
@@ -10,17 +10,17 @@ class Applicant(models.Model):
         (u'3', u'Third Year'),
         (u'4', u'Fourth Year'),)
 
-    EXPERIENCES = (
-        (u'0', u'The snake? (None)'),
-        (u'1', u'Heard once about it (Beginner)'),
-        (u'2', u'I use it all the time (Experienced)'),
-        (u'3', u'!! Pro Pythonista !!'),)
+    # EXPERIENCES = (
+    #     (u'0', u'The snake? (None)'),
+    #     (u'1', u'Heard once about it (Beginner)'),
+    #     (u'2', u'I use it all the time (Experienced)'),
+    #     (u'3', u'!! Pro Pythonista !!'),)
 
     registered_at     = models.DateTimeField(auto_now_add=True)
     workshop          = models.CharField(max_length=20, choices=WORKSHOPS)
     name              = models.CharField(max_length=255)
     year              = models.CharField(max_length=1, choices=YEARS)
-    python_experience = models.CharField(max_length=1, choices=EXPERIENCES)
+    # python_experience = models.CharField(max_length=1, choices=EXPERIENCES)
     phone_number      = models.CharField(max_length=10, blank=True)
     email             = models.EmailField()
     solved_puzzle     = models.BooleanField(default=False)

d32927184f98ee6cfbbb68de989be8d6c5a55b17
Sourabh Verma <sourabh.coder@gmail.com>
2014-10-30 23:10:56 +0530

    Add Android Workshop

--- registrar/workshops.py
+++ registrar/workshops.py
@@ -1,10 +1,13 @@
 WORKSHOPS = (
-    (u'dropbox', u'Dropbox CLI Tool'),
+    (u'android', u'Android Game Development'),
     (u'pygame', u'Python-Pygame Workshop'),
-    (u'blog', u'Jekyll Powered Blog'),)
+    (u'blog', u'Jekyll Powered Blog'),
+    (u'dropbox', u'Dropbox CLI Tool'),
+)
 
 WORKSHOPS_TEMPLATES = {
     'dropbox': 'registrar/workshops/dropbox.html',
     'pygame': 'registrar/workshops/pygame.html',
     'blog': 'registrar/workshops/blog.html',
+    'android': 'registrar/workshops/android.html',
 }
